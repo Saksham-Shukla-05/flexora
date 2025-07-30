ad25dd045c2adb4d9142814dd1cc710b7c0e403f
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-30 23:58:11 +0530

    Change some more file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="bg-red-500 text-white">hello</h1>;
+  return <div className="bg-red-500 text-white">hello</div>;
 }
 
 export default App;
